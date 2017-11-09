412627db011ad053b536066344e10b61b2d62a5c
EAM seonoh <eso0609@naver.com>
2017-11-09 19:18:25 +0900

    seonoh commit 1109

---
+++ app/src/main/java/com/example/ankotest/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ankotest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        verticalLayout {
+            val name = editText{
+                hint = "Please input your name"
+                textSize=20f
+            }
+
+            button("AnkoButton"){
+                onClick { toast("Hello Anko user, ${name.text}!!!") }
+            }
+        }
+
+        //  setContentView(R.layout.activity_main) //Anko를 사용하는 경우 xml을 따로 생성하지 않아도 되기때문
+        //  layout -> activity_main 필요없다 !!
+    }
+}
